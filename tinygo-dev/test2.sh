161159ef79c22800871af0150c4d394fe70a8e00
Brad Peabody <brad@peabody.io>
2019-11-22 10:35:12 -0800

    fix test build

--- tinygo-dev/test2.sh
+++ tinygo-dev/test2.sh
@@ -2,26 +2,16 @@
 
 set -e
 
-#cd ..
+# copy to src dir
+mkdir -p src/github.com/vugu/vugu
+rsync -taz --exclude=tinygo-dev --exclude=.git ../ src/github.com/vugu/vugu/
 
-TG=~/go/bin/tinygo
-
+# env and dependencies
 export GOPATH=`pwd`
 export GO111MODULE=off
-#go get github.com/vugu/vjson
-#go get github.com/vugu/xxhash
+go get github.com/vugu/vjson
+go get github.com/vugu/xxhash
+
+# compile
+TG=~/go/bin/tinygo
 $TG build -o testpgm2.wasm -target wasm ./
-
-#docker run --rm -it -v `pwd`/tinygo-dev:/go/src/testpgm tinygotest tinygo help
-
-# build docker image with dependent packages downloaded
-#docker build -t tinygotest -f tinygo-dev/Dockerfile .
-#docker run --rm -it -v `pwd`/tinygo-dev:/go/src/testpgm -e "GOPATH=/go" tinygotest \
-# tinygo build -o /go/src/testpgm/testpgm.wasm -target wasm testpgm
-
-# copy wasm_exec.js out
-#if ! [ -f tinygo-dev/wasm_exec.js ]; then
-#echo "Copying wasm_exec.js"
-#docker run --rm -it -v `pwd`/tinygo-dev:/go/src/testpgm tinygotest /bin/bash -c "cp /usr/local/tinygo/targets/wasm_exec.js /go/src/testpgm/"
-#fi
-
