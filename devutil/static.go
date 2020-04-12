b8b5f14880ddaa1f22ef8a4e483d91576eac325a
Brad Peabody <brad@peabody.io>
2020-04-12 12:36:02 -0700

    auto reloader static page

--- devutil/static.go
+++ devutil/static.go
@@ -8,21 +8,24 @@ import (
 )
 
 // DefaultIndex is the default index.html content for a development Vugu app.
+// The exact text `<title>Vugu App</title>`, `<!-- styles -->` and `<!-- scripts -->`
+// are meant to be replaced as needed if you quickly need to hack in CSS or
+// JS references for a development Vugu application.  If you need more control
+// than that, just copy it into your application.
 var DefaultIndex = StaticContent(`<!doctype html>
 <html>
 <head>
 <title>Vugu App</title>
 <meta charset="utf-8"/>
-<!-- TODO: should we include bootstrap or something on the default page? 
-<link rel="stylesheet" href="" />
--->
-<script src="https://cdn.jsdelivr.net/npm/text-encoding@0.7.0/lib/encoding.min.js"></script> <!-- MS Edge polyfill -->
-<script src="/wasm_exec.js"></script>
+<!-- styles -->
 </head>
 <body>
 <div id="vugu_mount_point">
 <img style="position: absolute; top: 50%; left: 50%;" src="https://cdnjs.cloudflare.com/ajax/libs/galleriffic/2.0.1/css/loader.gif">
 </div>
+<script src="https://cdn.jsdelivr.net/npm/text-encoding@0.7.0/lib/encoding.min.js"></script> <!-- MS Edge polyfill -->
+<script src="/wasm_exec.js"></script>
+<!-- scripts -->
 <script>
 var wasmSupported = (typeof WebAssembly === "object");
 if (wasmSupported) {
@@ -32,28 +35,33 @@ if (wasmSupported) {
 			return await WebAssembly.instantiate(source, importObject);
 		};
 	}
-	const go = new Go();
-	WebAssembly.instantiateStreaming(fetch("/main.wasm"), go.importObject).then((result) => {
-		go.run(result.instance);
-	});
+	var mainWasmReq = fetch("/main.wasm").then(function(res) {
+		if (res.ok) {
+			const go = new Go();
+			WebAssembly.instantiateStreaming(res, go.importObject).then((result) => {
+				go.run(result.instance);
+			});		
+		} else {
+			res.text().then(function(txt) {
+				var el = document.getElementById("vugu_mount_point");
+				el.style = 'font-family: monospace; background: black; color: red; padding: 10px';
+				el.innerText = txt;
+			})
+		}
+	})
 } else {
 	document.getElementById("vugu_mount_point").innerHTML = 'This application requires WebAssembly support.  Please upgrade your browser.';
 }
 </script>
-<!--
-
-TODO: in the index page's loader code we should include some logic to dump
-the contents of a non-200 response into a div so we can see it, or
-something - this way we can keep our structure but get pretty error 
-messages from the wasm compiler on-screen;
-
-TODO: script include for vgrun?  hm, think through if there will be an issue
-with this accidentally ending up live and if we need some "if localhost" logic.
-
--->
 </body>
 </html>
 `)
+
+// DefaultAutoReloadIndex is like DefaultIndex but also includes a script tag to load
+// auto-reload.js from the default URL.
+var DefaultAutoReloadIndex = DefaultIndex.Replace(
+	"<!-- scripts -->",
+	"<script src=\"http://localhost:8324/auto-reload.js\"></script>\n<!-- scripts -->")
 
 var startupTime = time.Now()
 
@@ -63,6 +71,11 @@ type StaticContent string
 // ServeHTTP implements http.Handler
 func (sc StaticContent) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	http.ServeContent(w, r, r.URL.Path, startupTime, strings.NewReader(string(sc)))
+}
+
+// Replace performs a single string replacement on this StaticContent and returns the new value.
+func (sc StaticContent) Replace(old, new string) StaticContent {
+	return StaticContent(strings.Replace(string(sc), old, new, 1))
 }
 
 // StaticFilePath implements http.Handler and serves the file at this path.
