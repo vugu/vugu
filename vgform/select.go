08f71c6bf6f2921f364998aebc5907546dec8ff8
Brad Peabody <brad@peabody.io>
2020-04-20 20:04:54 -0700

    some more stuff working on vgform

--- vgform/select.go
+++ vgform/select.go
@@ -26,72 +26,82 @@ import (
 // as appropriate.  See SliceOptions and MapOptions
 // for convenient adapters for []string and map[string]string.
 type Select struct {
-	Value   StringValuer // get/set the currently selected value
-	Options Options      // provide KeyLister and TextMapper in one
-	// AttrMap map[string]interface{} // regular HTML attributes like id and class
+	Value StringValuer // get/set the currently selected value
+	// TODO: should we also just make a ValuePtr *string - which would let people
+	// do :ValuePtr="&c.SomeRegularString" - seems like some people will want the convenience
+	// TODO: multiple (will need a new field and a new type, StringSliceValuer?)
+
+	Options Options // provide KeyLister and TextMapper in one
+
 	AttrMap vugu.AttrMap // regular HTML attributes like id and class
+	// TODO: might make sense to refactor this AttrMap thing to work well
+	// with SetAttributeInterface and AttributeLister/vg-attr - perhaps a slice of attributes
+	// is better and for vg-attr we just append to the slice and for the
+	// others we us SetAttributeInterace (or some variation that uses the same
+	// logic but works on an list of attributes.)  A type AttributeList []VGAttribute
+	// might be in order.  Refactor to separate vgnode package might be appropriate...
 
 	// el     js.Value
 	keys   []string
 	curVal string
 }
 
-func (s *Select) buildKeys() []string {
+func (c *Select) buildKeys() []string {
 
-	// if s.el.IsUndefined() {
-	// 	panic(errors.New("Select should have s.el set"))
+	// if c.el.IsUndefined() {
+	// 	panic(errors.New("Select should have c.el set"))
 	// }
-	if s.Value == nil {
+	if c.Value == nil {
 		panic(errors.New("Select.Value must not be nil"))
 	}
-	if s.Options == nil {
+	if c.Options == nil {
 		panic(errors.New("Select.Options must not be nil (TODO: when slots are supported that will be allowed instead of Options)"))
 	}
 
-	s.keys = s.Options.KeyList()
-	s.curVal = s.Value.StringValue()
+	c.keys = c.Options.KeyList()
+	c.curVal = c.Value.StringValue()
 
-	return s.keys
+	return c.keys
 }
 
-func (s *Select) isOptSelected(k string) bool {
-	return s.curVal == k
+func (c *Select) isOptSelected(k string) bool {
+	return c.curVal == k
 }
 
-func (s *Select) optText(k string) string {
-	return s.Options.TextMap(k)
+func (c *Select) optText(k string) string {
+	return c.Options.TextMap(k)
 }
 
-func (s *Select) handlePopulate() {
+func (c *Select) handlePopulate() {
 
 	// var buf bytes.Buffer
-	// buf.Grow(len(`<option value=""></option>`) * len(s.keys) * 2)
-	// for _, k := range s.keys {
+	// buf.Grow(len(`<option value=""></option>`) * len(c.keys) * 2)
+	// for _, k := range c.keys {
 	// 	buf.WriteString(`<option value="`)
 	// 	buf.WriteString(html.EscapeString(k))
 	// 	buf.WriteString(`">`)
-	// 	buf.WriteString(html.EscapeString(s.Options.TextMap(k)))
+	// 	buf.WriteString(html.EscapeString(c.Options.TextMap(k)))
 	// 	buf.WriteString(`</option>`)
 	// }
-	// s.el.Set("innerHTML", buf.String())
+	// c.el.Set("innerHTML", buf.String())
 
 	// see if we need this...
-	// v := s.curVal
+	// v := c.curVal
 	// idx := -1
-	// for i, k := range s.keys {
+	// for i, k := range c.keys {
 	// 	if k == v {
 	// 		idx = i
 	// 	}
 	// }
 
-	// s.el.Set("selectedIndex", idx)
+	// c.el.Set("selectedIndex", idx)
 
 }
 
-func (s *Select) handleChange(event *vugu.DOMEvent) {
+func (c *Select) handleChange(event *vugu.DOMEvent) {
 
 	newVal := event.PropString("target", "value")
-	s.curVal = newVal // why not
-	s.Value.SetStringValue(newVal)
+	c.curVal = newVal // why not
+	c.Value.SetStringValue(newVal)
 
 }
--- vgform/values.go
+++ vgform/values.go
@@ -8,6 +8,15 @@ type StringValuer interface {
 	SetStringValue(string)
 }
 
+// TODO: hm, would this not be better as:
+// type StringValue string
+// func (s *StringValue) StringValue() string {
+// This would allow people to either cast a *string to *StringValue
+// or just use vgform.StringValue directly as a struct member and
+// pass it's address right into the Value property, i.e.
+// < ... :Value="&c.SomeStringValue">
+// Maybe a StrPtr(&c.RegularString) would be good.
+
 // NOTE: It is useful to do `StringPtr{something}` so give careful
 // thought before adding another field to StringPtr.
 // StringPtr must be a struct because you cannot add methods to
