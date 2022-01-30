40689936b554835c93053984dec770e66a6ee525
Søren L. Hansen <sorenisanerd@gmail.com>
2022-01-30 11:22:35 -0800

    Add fallback to String() to accommodate tinygo
    fmt.Print relies on a deferred recover() to catch nil pointer
    dereferences in String() calls. Tinygo does not support this at the
    moment, so we need to explicitly handle it in the String() method.

--- wasm-test-suite/test-014-attrintf/root.go
+++ wasm-test-suite/test-014-attrintf/root.go
@@ -1,31 +1,34 @@
 package main
 
 type TestStringer struct {
-    str string
+	str string
 }
 
 func (s *TestStringer) String() string {
-    return s.str
+	if s == nil {
+		return ""
+	}
+	return s.str
 }
 
 type Root struct {
-    StringVar      string
-    IntVar         int
-    TrueVar        bool
-    FalseVar       bool
-    StringNilPtr   *string
-    Stringer       *TestStringer
-    StringerNilPtr *TestStringer
+	StringVar      string
+	IntVar         int
+	TrueVar        bool
+	FalseVar       bool
+	StringNilPtr   *string
+	Stringer       *TestStringer
+	StringerNilPtr *TestStringer
 }
 
 func (c *Root) BeforeBuild() {
-    c.StringVar = "aString"
-    c.IntVar = 42
-    c.TrueVar = true
-    c.FalseVar = false
-    c.StringNilPtr = nil
-    c.Stringer = &TestStringer{
-        str: "myString",
-    }
-    c.StringerNilPtr = nil
+	c.StringVar = "aString"
+	c.IntVar = 42
+	c.TrueVar = true
+	c.FalseVar = false
+	c.StringNilPtr = nil
+	c.Stringer = &TestStringer{
+		str: "myString",
+	}
+	c.StringerNilPtr = nil
 }
