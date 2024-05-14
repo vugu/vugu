dd2d8cfd5d4a6bdbb0865c3dc6e1b5aee1ed1556
Owen Waller <owenwaller@users.noreply.github.com>
2024-05-14 17:04:47 +0100

    Added the refactored version of test test-009-trim-unused from the wasm test suite
    Added the refactored version of test test-009-trim-unused from teh wasm
    test suite.

---
+++ new-tests/test-009-trim-unused/main_wasm.go
@@ -0,0 +1,45 @@
+//go:build wasm
+// +build wasm
+
+package main
+
+import (
+	"fmt"
+
+	"github.com/vugu/vugu"
+	"github.com/vugu/vugu/domrender"
+)
+
+func main() {
+
+	var mountPoint *string
+	{
+		mp := "#vugu_mount_point"
+		mountPoint = &mp
+	}
+
+	fmt.Printf("Entering main(), -mount-point=%q\n", *mountPoint)
+
+	renderer, err := domrender.New(*mountPoint)
+	if err != nil {
+		panic(err)
+	}
+
+	buildEnv, err := vugu.NewBuildEnv(renderer.EventEnv())
+	if err != nil {
+		panic(err)
+	}
+
+	rootBuilder := &Root{}
+
+	for ok := true; ok; ok = renderer.EventWait() {
+
+		buildResults := buildEnv.RunBuild(rootBuilder)
+
+		err = renderer.Render(buildResults)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+}
