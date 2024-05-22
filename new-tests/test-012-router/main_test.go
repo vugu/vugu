8d918f38d8fc705c65d32855256891e3fe1106ea
Owen Waller <owenwaller@users.noreply.github.com>
2024-05-22 11:49:26 +0100

    FIXME: Add a comment to test-012-router that describes why the test is incomplete
    The refactored test test-012-router from the (original) wasm-test-suite is
    currently incomplete. This commit adds a FIXME comment to the code at
    the failure point.
    
    The problem is that the hard coded HTML hrefs in the page1/page2.vugu work,
    but the links that are generated via the vgrouter and are called in response
    to the button clicks return a 404 (or timeout).
    
    This can be seen by connecting to the nginx container locally with a web
    browser.
    
    The revised version addes a URL path prefix of "/test-012-router". It is
    unclear if this prefix which cannot be removed, as it related to the
    filesystem path that nginx is using to serve the wasm, is causing the
    vgrouter to fail or not.
    
    This test therefore needs to be corrected, as the original
    wasm-test-suite vetrsion of test-012-router does pass.

--- new-tests/test-012-router/main_test.go
+++ new-tests/test-012-router/main_test.go
@@ -23,6 +23,15 @@ func Test012Router(t *testing.T) {
 
 	// rack the forward/back history stuff and replace option vs not and make sure that all works right
 	// with fragment mode and without
+
+	// FIXME This test, based on the original wasm test suite (test-012-router) is currently incomplete.
+	// The problem is that the hard coded HTML hrefs in the page1/page2.vugu work, but the links that
+	// are generated via the vgrouter and are called in response to the button clicks return a 404 (or timeout).
+	// This can be seen by connecting to the nginx container locally with a web browser.
+	// See the magefile for details of how to start the nginx docker container locally.
+	// The wasm will be served with a path prefix of "/test-012-router". It's unclear if this is
+	// related to the vgrouter routing at this stage.
+	// The failure mode applies to both the non-fragment and fragment versions.
 	//var tmpres []byte
 
 	// regular version
@@ -31,6 +40,7 @@ func Test012Router(t *testing.T) {
 		chromedp.WaitVisible("#page1"), // make sure page1 showed initially
 		chromedp.Click("#page2_link"),  // regular a href link
 		chromedp.WaitVisible("#page2"), // make sure it loads
+		// FIXME Uncomment the Click(...) and WaitVisible(...) lines to see the failure mode.
 		// chromedp.Click("#page1_button"), // button goes to page1 without a reload
 		// chromedp.WaitVisible("#page1"),  // make sure it renders
 		// chromedp.Click("#page2_button_repl"),                   // go to page2 without adding to history
@@ -47,6 +57,7 @@ func Test012Router(t *testing.T) {
 	chromedpHelper.Must(chromedp.Run(ctx,
 		chromedp.Navigate(url+"#/"),    // the test has detection code that sees the fragment here and puts it into fragment mode
 		chromedp.WaitVisible("#page1"), // make sure page1 showed initially
+		// FIXME Uncomment the Evaluate(...) and WaitVisible(...) lines to see the failure mode.
 	// 	chromedp.Evaluate("window.location='#page2'", &tmpres), // browse to page2 via fragment
 	// 	chromedp.WaitVisible("#page2"),                         // make sure it renders
 	// 	chromedp.Click("#page1_button"),                                         // button goes to page1 without a reload
