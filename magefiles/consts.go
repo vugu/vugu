4142d876c9d0422415b49ac4a6c16540c3687850
Owen Waller <owenwaller@users.noreply.github.com>
2024-05-25 20:52:23 +0100

    Rename the magefile targets and add the target documentation.
    Update the magefile by:
    
    Redefining 'All` so that it no longer runs the legacy wasm test suite.
    Instead it always runs the newly refactored wasm test suite.
    
    The legacy wasm test suite is only run by explicitly calling the
    'TestLegacyWasm' target or the 'AllWithLegacyWasm' target. Both of these
    targets are new.
    
    The `AllFast' target has been removed.
    
    The `AllFastNoLint` target has been renamed to `AllNoLint`.
    
    The 'Build' target no longer checks for the presense of the docker
    command - it never needed to in the first place.
    
    Added and updated the docuemtnation for each target as displayed via the
    `mage -l` command.
    
    The constants used in the magefile are now in a separate const.go file.

---
+++ magefiles/consts.go
@@ -0,0 +1,16 @@
+package main
+
+const GoLangCiLintImageName = "golangci/golangci-lint:latest-alpine"
+const TinyGoImageName = "tinygo/tinygo:latest"
+const LegacyWasmTestSuiteImageName = "vugu/wasm-test-suite:latest"
+const NginxImageName = "nginx:latest"
+const ChromeDpHeadlessShellImageName = "chromedp/headless-shell:latest"
+
+const LegacyWasmTestSuiteContainerName = "wasm-test-suite"
+const VuguNginxContainerName = "vugu-nginx"
+const VuguChromeDpContainerName = "vugu-chromedp"
+
+const VuguContainerNetworkName = "vugu-net"
+
+const WasmTestSuiteDir = "wasm-test-suite"
+const LegacyWasmTestSuiteDir = "legacy-wasm-test-suite"
