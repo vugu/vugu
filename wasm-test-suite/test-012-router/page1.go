e93cb165c64ea72eb502d9c2c89ac05c6c84c2bb
Owen Waller <owenwaller@users.noreply.github.com>
2025-04-15 14:36:10 +0100

    Refactor the test to remove the need for <script type="application/x-go"> tags
    Refactored the test to remove the need for the
    
    <script type="application/x-go">
    
    tags in the *.vugu files.
    
    The script tag was previously required because the constant
    vgrouter.NavReplace wss referenced directly in the parameter list
    of the Navigate function.

--- wasm-test-suite/test-012-router/page1.go
+++ wasm-test-suite/test-012-router/page1.go
@@ -5,3 +5,11 @@ import "github.com/vugu/vgrouter"
 type Page1 struct {
 	vgrouter.NavigatorRef
 }
+
+func (c *Page1) NavigateToPage(url string) {
+	c.Navigate(url, nil)
+}
+
+func (c *Page1) NavigateToPageWithReplace(url string) {
+	c.Navigate(url, nil, vgrouter.NavReplace)
+}
