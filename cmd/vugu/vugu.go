59426d569f329cefae70c132e36d227a8656eed0
Owen Waller <owenwaller@users.noreply.github.com>
2026-07-01 19:21:02 +0100

    Update the help test for the 'vugu' command and subcommands.
    Update the help text for the:
    
    `vugu help`
    `vugu help init`
    `vugu help gen`
    
    commands.
    The change largely corrects the USAGE sections, to correctly
    display the options and any required arguments for each sub command.

--- cmd/vugu/vugu.go
+++ cmd/vugu/vugu.go
@@ -16,7 +16,7 @@ import (
 // The root `vugu` command entry point.
 func main() {
 	// Calculate the version string.
-	// We want this as a separate string variable becase we need to assign it to the
+	// We want this as a separate string variable because we need to assign it to the
 	// cli.Command.Version field. Any errors need to be handled here.
 	v, err := version.VersionString()
 	if err != nil {
@@ -37,7 +37,9 @@ func main() {
 		version.Version(context.Background(), cmd)
 	}
 	cmd := &cli.Command{
-		Version: v, // the version is printed under the VERSION: section if `vugu` is called without arguments.
+		Version:   v, // the version is printed under the VERSION: section if `vugu` is called without arguments.
+		Usage:     "bootstraps and builds a vugu project",
+		ArgsUsage: "[GO_MODULE_NAME]",
 		Commands: []*cli.Command{
 			// The version sub command. See the version.Version
 			// Also called in response to the "-v" and "--version" flags.
@@ -48,9 +50,10 @@ func main() {
 				Action:  version.Version,
 			},
 			{
-				Name:    "gen",
-				Aliases: []string{"g"},
-				Usage:   "Generate code",
+				Name:      "gen",
+				Aliases:   []string{"g"},
+				Usage:     "Generate the Go code from the .vugu files in the directory",
+				ArgsUsage: "[OPTIONS] DIRECTORY",
 				Flags: []cli.Flag{
 					&cli.BoolFlag{
 						Name:        "skip-go-mod",
@@ -61,7 +64,7 @@ func main() {
 					&cli.BoolFlag{
 						Name:        "skip-main",
 						Value:       false,
-						Usage:       "Donot try to create main.go as needed",
+						Usage:       "Do not try to create main.go as needed",
 						Destination: &gen.Opts.SkipMainGo,
 					},
 					&cli.BoolFlag{
@@ -86,9 +89,11 @@ func main() {
 				Action: gen.Gen,
 			},
 			{
-				Name:    "init",
-				Aliases: []string{"i"},
-				Usage:   "Bootstrap a new vugu project",
+				Name:      "init",
+				Aliases:   []string{"i"},
+				Usage:     "Bootstrap a new vugu project",
+				ArgsUsage: "[OPTIONS] GO_MODULE_NAME",
+				//ArgsUsage: "GO_MODULE_NAME",
 				Flags: []cli.Flag{
 					&cli.StringFlag{
 						Name:        "dir",
