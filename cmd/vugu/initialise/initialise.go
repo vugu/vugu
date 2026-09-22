d88142da66fbcc865868c77dea201bff9f7105bc
Owen Waller <owenwaller@users.noreply.github.com>
2026-09-22 15:39:53 +0100

    Fix: ensure that vugu init produces code that can be built
    The templates used by the vugu init command contains syntax errors. This
    results in the code failing to build.
    
    The templates have now been corrected, and an additional make target
    TestVuguInit added. TestVuguInit builds the code produced by vugu init
    and vugu gen, ensuring that there are no syntax errors.
    
    This bug got though because the existing testing was insufficient.
    Adding the new TestVuguInit target closes that gap.
    
    See:
    https://github.com/vugu/vugu/issues/428
    
    This commit only fixes the v0.5.0 module. A similar but not identical
    fix will need to be applied to the v2.y.z module.

--- cmd/vugu/initialise/initialise.go
+++ cmd/vugu/initialise/initialise.go
@@ -188,8 +188,6 @@ func createMainWasmDotGo(fs fs.FS, opts InitOpts) error {
 	}
 	// now we need the just the package name from the full import path.
 	// we can use filepath.Base for this
-	fmt.Printf("Before %q\n", opts.RootStructPkgAlias)
-
 	if opts.RootStructPkgAlias == "" {
 		// the package has not been aliased so we need to get the package name from the import path
 		opts.RootStructPkgAlias = filepath.Base(opts.RootStructPkgImportPath)
@@ -198,7 +196,6 @@ func createMainWasmDotGo(fs fs.FS, opts InitOpts) error {
 			opts.RootStructPkgAlias = ""
 		}
 	}
-	fmt.Printf("After %q\n", opts.RootStructPkgAlias)
 
 	err = tmpl.Execute(mainWasm, opts)
 	if err != nil {
